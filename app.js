453e8efd4f83a1192938ada5629c2d81b50994df
Stephen McLeod <stephen.m.mcleod@gmail.com>
2013-08-21 00:39:48 -0400

    add express skeleton, neurosky listener

---
+++ app.js
@@ -0,0 +1,57 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var nodeThinkGear = require('node-neurosky');
+
+var tgClient = nodeThinkGear.createClient({
+	appName:'NodeThinkGear',
+	appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+});
+
+tgClient.on('data',function(data){
+	console.log(data);
+});
+
+tgClient.connect();
+
+
+app.get('/', routes.index);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
--- server.js
+++ server.js
@@ -1,6 +1,37 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require("express");
+var app = express()
+	, server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+
+app.listen(8003);
+console.log('Listening on port 8003');
+
+
+ 
+var nodeThinkGear = require('node-neurosky');
+
+var tgClient = nodeThinkGear.createClient({
+	appName:'NodeThinkGear',
+	appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+});
+
+tgClient.on('data',function(data){
+	console.log(data);
+});
+
+tgClient.connect();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World');
+// });
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
