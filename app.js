78649ccaa0d1c9744249c66f6909ab66052ae0ab
Stephen McLeod <stephen.m.mcleod@gmail.com>
2013-08-21 16:13:53 -0400

    add nib, update views

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var stylus = require('stylus');
+var nib = require('nib');
 
 var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
+/**
+ * Variables
+ */
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -19,13 +31,46 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
+
+
+
+app.get('/', routes.index);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+/**
+ * Socket.IO
+ */
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+
+
+/**
+ * ThinkGear
+ */
 
 var nodeThinkGear = require('node-neurosky');
 
@@ -39,19 +84,3 @@ tgClient.on('data',function(data){
 });
 
 tgClient.connect();
-
-
-app.get('/', routes.index);
-
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-var io = require('socket.io').listen(server);
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
