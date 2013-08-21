9b77c9d311fa5e68825c494e3eb416c22cf789f5
Stephen McLeod <stephen.m.mcleod@gmail.com>
2013-08-21 18:32:01 -0400

    move d3 js to layout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,18 @@
 var socket = io.connect('http://localhost:3000/');
+var waveData = {};
+var blinking = false;
 
-setInterval(function(){
-  socket.emit('getMindwave', { success: true });
-},100);
+// setInterval(function(){
+//   socket.emit('getMindwave', { success: true });
+// },100);
 
 socket.on('updateMindwave', function (data) {
+  if (data['blinkStrength'] == null) {
+    waveData = data;
+    blinking = false;
+  } else {
+    blinking = data['blinkStrength'];
+  }
   // console.log(data['eegPower']['highGamma']);
   // data['eSense']['attention']
   // data['eSense']['meditation']
